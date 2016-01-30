aba3ee10c22be09967d0d168baa38e11730c9e21
EphesosX <tkt5@njit.edu>
2016-01-30 17:04:39 -0500

    Added more trackers
    Cleaned up the tracker creation
    Attempted to create icons, but they aren't showing up. Being drawn over?

--- js/game_logic.js
+++ js/game_logic.js
@@ -1,8 +1,8 @@
 //PlayerStats is an array that holds all of the player's statistics
 game.playerStats=[];
 //Initialize all values
-game.playerStats.prayerPoints=new game.StatNumber(10000000);
-game.playerStats.cultists=new game.StatNumber(100000);
+game.playerStats.prayerPoints=new game.StatNumber(10000);
+game.playerStats.cultists=new game.StatNumber(1000);
 game.playerStats.prisoners=new game.StatNumber(0);
 game.playerStats.prodRateCult=new game.StatNumber(0);
 game.playerStats.prodRatePris=new game.StatNumber(0);
@@ -26,19 +26,12 @@ game.backgrounds=[];
 //This array holds all buttons
 game.buttons=[];
 
-//This array holds all UpgradeButtons, which are still in Buttons
-game.upgradeButtons=[];
+//This array holds all ToolButtons, which are still in Buttons
+game.toolButtons=[];
 
 game.tabs=[];
 
 console.log(window.innerWidth);
-
-var test=new game.Background(0,0,1920,1080,"img/bg3.png");
-//var testText=new game.Text(test,20,40,"","72pt serif","black");
-var testText3=new game.TextNumber(test,700,200,"0","bold 28pt lucida console ","white",6,"#5f3c0f",game.playerStats.prayerPoints);
-var testText4=new game.TextNumber(test,900,200,"0","bold 28pt lucida console ","white",6,"#5f3c0f",game.playerStats.cultists);
-var testText5=new game.TextNumber(test,1100,200,"0","bold 28pt lucida console ","white",6,"#5f3c0f",game.playerStats.prisoners);
-test.setVisible(true);
 
 var test2=new game.ToolButton(0,267,580,98,"img/conversion_book.png",game.playerStats.costBooksPP,game.playerStats.costBooksCult,0,0,0,game.playerStats.numBooks);
 var testText2=new game.Text(test2.background,150,5,"BOOK","bold 30pt lucida console ","white",6,"#5f3c0f");
@@ -48,10 +41,37 @@ test2.setVisible(true);
 
 
 game.buttons.push(test2);
-game.upgradeButtons.push(test2);
+game.toolButtons.push(test2);
 
-game.backgrounds.push(test);
 
+//This master background is behind everything
+//It holds objects that will always appear on the screen
+game.masterBackground=new game.Background(0,0,1920,1080,"img/bg3.png");
+game.masterBackground.setVisible(true);
+
+//Create the text number tracker for prayerPoints
+game.prayerPointsText=new game.TextNumber(game.masterBackground,700,200,"0","bold 28pt lucida console ","white",6,"#5f3c0f",game.playerStats.prayerPoints);
+//Create the icon for prayerPoints
+game.prayerPointsIcon=new game.Background(650,200,37,37,"img/coin.png");
+game.prayerPointsIcon.setVisible(true);
+game.backgrounds.push(game.prayerPointsIcon);
+
+//Create the text number tracker for cultists
+game.cultistsText=new game.TextNumber(game.masterBackground,900,200,"0","bold 28pt lucida console ","white",6,"#5f3c0f",game.playerStats.cultists);
+//Create the icon for cultists
+
+game.cultistsIcon=new game.Background(850,200,37,37,"img/happy.png");
+game.cultistsIcon.setVisible(true);
+game.backgrounds.push(game.cultistsIcon);
+
+//Create the text number tracker for prisoners
+game.prisonersText=new game.TextNumber(game.masterBackground,1100,200,"0","bold 28pt lucida console ","white",6,"#5f3c0f",game.playerStats.prisoners);
+//Create the icon for prisoners
+game.prisonersIcon=new game.Background(1050,200,37,37,"img/anger.png");
+game.prisonersIcon.setVisible(true);
+game.backgrounds.push(game.prisonersIcon);
+
+game.backgrounds.unshift(game.masterBackground);
 
 
 function button1Click(){
